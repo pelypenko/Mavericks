349b4d23bb2d2c67c5d778ae4f9916c1bcb65b8f
Oleg Semenchuk <oleg.semenchuk@intapp.com>
2017-08-31 16:58:51 +0300

    Add NewRequeastConversation bot

---
+++ CreateNewRequestConversation/CreateNewClientNewMatterConversationDialog.cs
@@ -0,0 +1,36 @@
+﻿namespace CreateNewConversationBot
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.Internals.Fibers;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class CreateNewClientNewMatterConversationDialog : IDialog<object>
+    {
+        private readonly ISurveyService surveyService;
+
+        public CreateNewClientNewMatterConversationDialog(ISurveyService surveyService)
+        {
+            SetField.NotNull(out this.surveyService, nameof(surveyService), surveyService);
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var value = await result;
+            var message = value.Text.ToLower();
+
+            await context.PostAsync("New request will start in a few seconds...");
+
+            await this.surveyService.QueueSurveyAsync();
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+    }
+}
